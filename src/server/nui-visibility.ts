164a9716419085e81dbc569ec9289aa0747c41e7
David Malchin <malchin459@gmail.com>
2025-11-13 21:47:41 +0200

    Fix formatting

--- src/server/nui-visibility.ts
+++ src/server/nui-visibility.ts
@@ -1,5 +1,5 @@
 import { ServerNetEvent } from '@common/net-events';
-import { registerNetEvent } from './utils/net';
+import { registerNetEvent } from './utils/net.ts';
 
 registerNetEvent(ServerNetEvent.OpenMenu, () => {
     return [IsPlayerAceAllowed(`${source}`, 'devutils.open')];
